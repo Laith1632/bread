f1d82d7777c1d7d127eab71f266b8998f9409b81
Laith Albusairi <laithalbusairi@gmail.com>
2022-09-19 23:04:50 -0700

    addededit jsx and button in show

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -26,10 +26,17 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
-
 // NEW
 breads.get('/new', (req, res) => {
   res.render('new')
+})
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
 })
 
 // SHOW
@@ -44,12 +51,25 @@ breads.get('/:arrayIndex', (req, res) => {
   }
 })
 
-
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+
+
 
 
 
